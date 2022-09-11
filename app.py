ecd7facb1433bfcc8cdafc2ee2193111acec0a0c
Kevin <kevinparragalarza@gmail.com>
2022-09-11 13:04:01 +0200

    probando 7, modificado output de app

--- app.py
+++ app.py
@@ -108,6 +108,7 @@ app.layout = html.Div([
     Output('facebook-visit', 'children'),
     Output('instagram-visit', 'children'),
     Output('twitter-visit', 'children'),
+    Output('twitch-visit', 'children')
     Output('total-visit-line', 'figure'),
     Output('total-visit-social-networks-line', 'figure'),
     Output('world-map', 'figure'),
@@ -147,15 +148,6 @@ def update_figures(start_date_selected, end_date_selected, social_networks_selec
     twitter_visit = (
         df
         .loc[(df.social_network == 'twitter') &
-             (df.social_network.isin(social_networks_selected)) &
-             (df.device.isin(devices_selected)) &
-             (df.datetime >= start_date_selected) &
-             (df.datetime <= end_date_selected)]
-    ).shape[0]
-    
-    twitch_visit = (
-        df
-        .loc[(df.social_network == 'twitch') &
              (df.social_network.isin(social_networks_selected)) &
              (df.device.isin(devices_selected)) &
              (df.datetime >= start_date_selected) &
@@ -255,7 +247,7 @@ def update_figures(start_date_selected, end_date_selected, social_networks_selec
         }
     )
 
-    return total_visit, facebook_visit, instagram_visit, twitter_visit, twitch_visit, total_visit_fig, total_visit_social_network_fig, world_map_fig, devices_pie_fig
+    return total_visit, facebook_visit, instagram_visit, twitter_visit, total_visit_fig, total_visit_social_network_fig, world_map_fig, devices_pie_fig,
 
 
 if __name__ == '__main__':
